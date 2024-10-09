cc0a769af81a6f59a902a1e483668ba33c6926eb
Kevin Hahn <kevin_hahn@sil.org>
2024-10-09 13:54:36 +0700

    remove IObjectBase constraint on DeleteChange

--- src/SIL.Harmony/Changes/DeleteChange.cs
+++ src/SIL.Harmony/Changes/DeleteChange.cs
@@ -3,13 +3,13 @@
 namespace SIL.Harmony.Changes;
 
 public class DeleteChange<T>(Guid entityId) : EditChange<T>(entityId), IPolyType
-    where T : class, IPolyType, IObjectBase
+    where T : class
 {
-    public static string TypeName => "delete:" + T.TypeName;
+    public static string TypeName => "delete:" + typeof(T).Name;
 
     public override ValueTask ApplyChange(T entity, ChangeContext context)
     {
-        entity.DeletedAt = context.Commit.DateTime;
+        context.Adapt(entity).DeletedAt = context.Commit.DateTime;
         return ValueTask.CompletedTask;
     }
 }

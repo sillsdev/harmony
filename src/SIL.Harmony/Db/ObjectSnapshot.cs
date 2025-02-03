af8d9de68699328f7089933367b2f5e90aea86f8
Robin Munn <rmunn@pobox.com>
2025-02-03 11:26:45 -0500

    Create IObjectSnapshot interface in core
    ObjectSnapshot relies on too many things from the DB to be moved to core
    easily, so we move it back to SIL.Harmony.Db and create an interface in
    core instead. Then the only difference is that the type of Commit will
    be different depending on whether you access it via the interface (in
    which case you'll get a CommitBase from core) or via the implementation
    class (in which case you'll get a Commit object from SIL.Harmony).

--- src/SIL.Harmony/Db/ObjectSnapshot.cs
+++ src/SIL.Harmony/Db/ObjectSnapshot.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 using SIL.Harmony.Core;
 using SIL.Harmony.Entities;
 using SIL.Harmony.Helpers;
@@ -22,8 +24,58 @@ public record SimpleSnapshot(
         snapshot.CommitId,
         snapshot.IsRoot,
         snapshot.Commit.HybridDateTime,
-        snapshot.Commit.Hash, // PROBLEM: ObjectSnapshot.Commit is now a CommitBase which doesn't have a Hash property
+        snapshot.Commit.Hash,
         snapshot.EntityIsDeleted)
     {
     }
+}
+
+public class ObjectSnapshot : IObjectSnapshot
+{
+    public static ObjectSnapshot ForTesting(Commit commit)
+    {
+        return new ObjectSnapshot
+        {
+            Commit = commit,
+            Entity = null!,
+            Id = Guid.Empty,
+            References = [],
+            CommitId = commit.Id,
+            EntityId = Guid.Empty,
+            IsRoot = false,
+            TypeName = "Test",
+            EntityIsDeleted = false
+        };
+    }
+    //determines column name used in projected object tables, changing this will require a migration
+    public const string ShadowRefName = "SnapshotId";
+    [JsonConstructor]
+    protected ObjectSnapshot()
+    {
+    }
+
+    [SetsRequiredMembers]
+    public ObjectSnapshot(IObjectBase entity, Commit commit, bool isRoot) : this()
+    {
+        Id = Guid.NewGuid();
+        Entity = entity;
+        References = entity.GetReferences();
+        EntityId = entity.Id;
+        EntityIsDeleted = entity.DeletedAt.HasValue;
+        TypeName = entity.GetObjectTypeName();
+        CommitId = commit.Id;
+        Commit = commit;
+        IsRoot = isRoot;
+    }
+
+    public required Guid Id { get; init; }
+    public required string TypeName { get; init; }
+    public required IObjectBase Entity { get; init; }
+    public required Guid[] References { get; init; }
+    public required Guid EntityId { get; init; }
+    public required bool EntityIsDeleted { get; init; }
+    public required Guid CommitId { get; init; }
+    public required Commit Commit { get; init; }
+    CommitBase IObjectSnapshot.Commit => Commit;
+    public required bool IsRoot { get; init; }
 }

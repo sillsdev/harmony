eb41a22aeaede5fe452314d8d381ade3c5c63578
Tim Haasdyk <tim_haasdyk@sil.org>
2025-03-24 17:30:51 +0100

    Remove unused change-context props

--- src/SIL.Harmony/Changes/ChangeContext.cs
+++ src/SIL.Harmony/Changes/ChangeContext.cs
@@ -7,19 +7,15 @@ internal class ChangeContext : IChangeContext
     private readonly SnapshotWorker _worker;
     private readonly CrdtConfig _crdtConfig;
 
-    internal ChangeContext(Commit commit, int commitIndex, IDictionary<Guid, ObjectSnapshot> intermediateSnapshots, SnapshotWorker worker, CrdtConfig crdtConfig)
+    internal ChangeContext(Commit commit, SnapshotWorker worker, CrdtConfig crdtConfig)
     {
         _worker = worker;
         _crdtConfig = crdtConfig;
         Commit = commit;
-        CommitIndex = commitIndex;
-        IntermediateSnapshots = intermediateSnapshots;
     }
 
     CommitBase IChangeContext.Commit => Commit;
     public Commit Commit { get; }
-    public int CommitIndex { get; }
-    public IDictionary<Guid, ObjectSnapshot> IntermediateSnapshots { get; }
     public async ValueTask<IObjectSnapshot?> GetSnapshot(Guid entityId) => await _worker.GetSnapshot(entityId);
     public IAsyncEnumerable<object> GetObjectsReferencing(Guid entityId, bool includeDeleted = false)
     {

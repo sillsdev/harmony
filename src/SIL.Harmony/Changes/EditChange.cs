acb3c0819e99269b051f5a210e50b7a7e1253772
Kevin Hahn <kevin_hahn@sil.org>
2026-06-11 09:47:28 +0700

    Improve exception details and refactor unused method in change types

--- src/SIL.Harmony/Changes/EditChange.cs
+++ src/SIL.Harmony/Changes/EditChange.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
 namespace SIL.Harmony.Changes;
 
 public abstract class EditChange<T>(Guid entityId) : Change<T>(entityId)
@@ -6,6 +8,6 @@ public abstract class EditChange<T>(Guid entityId) : Change<T>(entityId)
     public override ValueTask<T> NewEntity(Commit commit, IChangeContext context)
     {
         throw new NotSupportedException(
-            $"type {GetType().Name} does not support NewEntity, because it inherits from {nameof(EditChange<T>)}, this means it must be called with a from an existing entity, not a newly generated one");
+            $"type {GetType().ShortDisplayName()} does not support NewEntity, because it inherits from {nameof(EditChange<T>)}, this means it must be called with a from an existing entity, not a newly generated one. CommitId: {commit.Id}, EntityId: {EntityId}");
     }
 }
